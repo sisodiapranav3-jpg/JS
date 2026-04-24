6c9349fedf3ceb680d77c5bb00e312bcd103f8a1
sisodiapranav3-jpg <sisodiapranav3@gmail.com>
2026-04-24 11:41:55 +0000

    operators

---
+++ 01/02_operators.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version. 
+// by default in new browsers it is always strict.
+
+let name = "pranav" // this is string data type
+let age = 18 // this is number 
+let isLoggedIn = false // this is boolean
+/*
+- readablility is must in code.
+- there are 7 data type.
+- number data tyoe range is => 2 to power 53.
+- bigint data tyoe => mostly unused.
+- boolean data type => true/false.
+- null data type => this is a type standalone  (eg - temperature is undifined).
+- undefined data type => it means that now value is not assigned.
+- Symbol data type =>  unique
+*/
+
+// object 
+
+console.log(typeof age);
+console.log(typeof null); // it gives o/p => object
+console.log(typeof undifined); // o/p => undifined
